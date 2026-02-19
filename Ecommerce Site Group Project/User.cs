73877988a1d09e8f8ce62cd552d7069a22ed45d1
plexlad <brock@deplex.us>
2026-02-18 20:49:00 -0700

    Added bare bones IUser interface and User class

--- Ecommerce Site Group Project/User.cs
+++ Ecommerce Site Group Project/User.cs
@@ -14,3 +14,48 @@ public enum Role
   Customer,
   Admin,
 }
+
+public interface IUser
+{
+    Guid Id { get; }
+    string Email { get; set; }
+    string UserName { get; set; }
+    string HashedPassword { set; }
+    string? Address { get; set; }
+    Role Role { get; set; }
+    Permissions Permissions { get; set; }
+
+    Task<bool> Register();
+    Task<bool> Login(string email, string password);
+    Task<bool> Update();
+}
+
+public class User : IUser
+{
+    public Guid Id { get; private set; }
+    public required string Email { get; set; }
+    public required string UserName { get; set; }
+    public required string HashedPassword { private get; set; }
+    public string? Address { get; set; }
+    public Role Role { get; set; }
+    public Permissions Permissions { get; set; }
+
+    public User()
+    {
+        Id = Guid.NewGuid();
+        Role = Role.Customer;
+        Permissions = Permissions.None;
+    }
+    public async Task<bool> Register()
+    {
+        return await Task.FromResult(false);
+    }
+    public async Task<bool> Login(string email, string password)
+    {
+        return await Task.FromResult(false);
+    }
+    public async Task<bool> Update()
+    {
+        return await Task.FromResult(false);
+    }
+}
